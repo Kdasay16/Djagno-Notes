ea05df5e22399736e1a2a5e1460942fd105da2d3
Kdasay16 <kayla@kayla-asay.com>
2022-09-13 11:58:10 -0700

    .env

--- Pizza-Project/pizzapalace/settings.py
+++ Pizza-Project/pizzapalace/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5w@+_l7))di9ywq465&i0zn$0t=-n8*ykt5ur&4bnfcqx1ug70'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
