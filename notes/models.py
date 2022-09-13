8027cfa19b4eee15fc271702eb9f253a8520b819
Kdasay16 <kayla@kayla-asay.com>
2022-09-12 18:13:42 -0700

    chaper 8- added auth

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,15 @@
 from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Notes(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="notes")
+ # The first item is the model we want to create a link with. In this case, this is the User model.
+ # Then, the second item is going to be the on_delete- what happens to this note if the user associated with it is deleted.
+ # models.CASCADE- if a user gets deleted, we also want to delete all the notes associated with them.
+ # Finally, we can say how we will identify this relationship on the user side. Related_name is going to be notes.
--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
-# from django.http import Http404
+from django.http.response import HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -12,28 +13,44 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     # form_class can be passed instead of 'fields' to allow for more powerful validation
     form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        # commit=False creates the object, but doesn't save it to the database
+        # this way we can insert the user before it goes to the database
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
 
 class NotesDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/notes'
+    login_url = "/admin"
 
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):  # get_queryset is a django method, used to get all USER notes
+        return self.request.user.notes.all()
 
 
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
+    login_url = "/admin"
 
 # This "function-based view" has been replaced with the NotesListView class to make this a "class based view"
 # def detail(request, pk):  # pk = private key
