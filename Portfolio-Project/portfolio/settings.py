c236d8cf4a70d5ee76b4233248bb52d7783c7bb0
Kdasay16 <kayla@kayla-asay.com>
2022-09-21 13:28:37 -0700

    loading job images

--- Portfolio-Project/portfolio/settings.py
+++ Portfolio-Project/portfolio/settings.py
@@ -126,9 +126,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+# Both static and media items below are added to urls.py
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# for dynamically loading images from Job
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
