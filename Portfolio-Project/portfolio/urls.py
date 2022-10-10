d2c01cf7cc2ac6a40ecae2b16825473db40a4d73
Kdasay16 <kayla@kayla-asay.com>
2022-10-10 16:03:15 -0700

    added details page, completed course

--- Portfolio-Project/portfolio/urls.py
+++ Portfolio-Project/portfolio/urls.py
@@ -23,6 +23,7 @@ import jobs.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.home, name='home'),
+    path('jobs/<int:job_id>', jobs.views.detail, name="details")
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
